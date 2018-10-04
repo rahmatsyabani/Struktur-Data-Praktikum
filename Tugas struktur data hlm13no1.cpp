53afdf82bf6db274a3bea8cd400a48af7d65fa05
rahmatsyabani <43659649+rahmatsyabani@users.noreply.github.com>
2018-10-04 16:53:44 +0700

    Add files via upload

---
+++ Tugas struktur data hlm13no1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+main(){
+
+	int r, s;
+	float rata_rata, t_akhir;
+	
+		cout<<"Masukkan jumlah angka= ";cin>>s;
+	int a[s];
+	#include <iostream>
+	for(r=0;r<s;r++){
+		cout<<"Masukkan angka ke:"<<1+r<<" = ";cin>>a[s];
+	t_akhir=t_akhir+a[s];
+	
+	}
+	
+	cout<<endl;
+	rata_rata=t_akhir/s;
+		cout<<"Hasil rata-ratanya = "<<rata_rata;
+	
+getch();
+
+
+}
